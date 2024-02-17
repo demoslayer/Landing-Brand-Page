cebb59d5da7cf33bcc99a03d63c7e0f25c49f98f
demoslayer <107707267+demoslayer@users.noreply.github.com>
2024-02-17 18:38:08 +0530

    Add files via upload

---
+++ src/Components/Content.js
@@ -0,0 +1,30 @@
+const Content=()=>{
+    return (
+<main className="hero container">
+    <div className="content">
+    <h1>ACHIEVE YOUR HEIGHT FOR FUTURE</h1>
+    <p>YOU ARE THE BEST YOU DONT KNOW THIS BECAUSE A BUTTERFLY CANNOT SEE ITS BEAUTIFUL WING, TRUST YOURSELF AND GO AHEAD</p>
+   <div className="btn"> 
+    <button >Shop Now</button>
+    <button className="category">Category</button>
+
+    </div>
+
+    <div className="shopping">
+        <p>Also available on</p>
+        <div className="brand-icons">
+            <img src="/images/flipkart.png" alt=""/>
+            <img src="/images/amazon.png" alt=""/>
+        </div>
+    </div>
+    </div>
+
+    <div className="content-image">
+  <img src="/images/tenniss.jpg" alt=""/>
+
+    </div>
+ </main>
+ 
+    );
+};
+export default Content;

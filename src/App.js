9853615e4eea78be3533560e87fb0b3e28a8ed5a
demoslayer <107707267+demoslayer@users.noreply.github.com>
2024-02-17 18:31:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+import './App.css'
+import Content from "./components/Content";
+const App=()=>{
+  return(
+
+<div>
+ <Navigation/>
+ <Content/>
+</div>
+
+
+  );
+};
+export default App;
